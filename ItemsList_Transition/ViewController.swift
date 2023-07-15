964469fda30f29c86374db41aa0cdc6e151c869e
MDA <>
2023-07-15 19:37:02 +0200

    initial commit

---
+++ ItemsList_Transition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ItemsList_Transition
+//
+//  Created by MDA on 15/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
